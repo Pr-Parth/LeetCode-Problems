de8ecaa02128052b83a67a8e2a4310c684548fde
Pr-Parth <89503982+Pr-Parth@users.noreply.github.com>
2023-06-29 22:31:27 +0530

    Add files via upload

---
+++ ccNewPiece.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,a,b,p,q;
+	cin>>t;
+	while(t--){
+	    
+	    cin>>a>>b>>p>>q;
+	 
+	    if(a==p && b==q){
+	        cout<<"0\n";
+	    }
+	    else if((a+b)%2== (p+q)%2){
+	        cout<<"2\n";
+	    }
+	    else {
+	        cout<<"1\n";
+	    }
+	    
+	}
+	return 0;
+}
