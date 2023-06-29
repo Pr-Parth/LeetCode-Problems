fe487820ec6d099422dc8bebe2e27b1b1f5488b8
Pr-Parth <89503982+Pr-Parth@users.noreply.github.com>
2023-06-29 22:33:22 +0530

    Add files via upload

---
+++ ccABSDIFF.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+// 	int t;
+// 	cin>>t;
+// 	while(t--){
+	    int A,B;
+	    cin>>A>>B;
+	    
+	    int x;
+	    x=A*B;
+	    int y;
+	    y=A+B;
+	    
+	    int z;
+	    z=abs(x-y);
+	    cout<<z<<endl;
+	
+	return 0;
+}
