5f8ca5fc12f661a7acaf11b1389489fc365c9e12
Pr-Parth <89503982+Pr-Parth@users.noreply.github.com>
2023-07-11 17:26:30 +0530

    Add files via upload

---
+++ ccElections.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    
+	    if(a>50 && b<50 && c<50){
+	        cout<<"A"<<endl;
+	    }
+	    else if(a<50 && b>50 && c<50){
+	        cout<<"B"<<endl;
+	    }
+	    else if(a<50 && b<50 && c>50){
+	        cout<<"C"<<endl;
+	    }
+	    else{
+	        cout<<"NOTA"<<endl;
+	    }
+	    
+	}
+	return 0;
+}
