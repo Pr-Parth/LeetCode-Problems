8bc109d9d4a17db24dd242c17d45462b5e951841
Pr-Parth <89503982+Pr-Parth@users.noreply.github.com>
2023-07-14 18:58:24 +0530

    Add files via upload

---
+++ ccEfficientPAN.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include<boost/multiprecision/cpp_int.hpp>
+#define ll long long
+using namespace boost::multiprecision;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int1024_t n;
+	    cin>>n;
+	    cout<<n%20<<endl;
+	    }
+	    
+	}
+
+
