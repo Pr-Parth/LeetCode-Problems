096d686fdaf6b42da9fea47080a4c187d33c15c5
Pr-Parth <89503982+Pr-Parth@users.noreply.github.com>
2023-08-27 00:59:29 +0530

    Add files via upload

---
+++ ccThreeFriends.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,z;
+	  cin>>x>>y>>z;
+	  if(x==y && y==z && x==z)
+	  cout<<"no"<<endl;
+	  else
+	  {
+	    if(x+y==z || x+z==y || y+z==x)
+	    cout<<"yes"<<endl;
+	    else
+	    cout<<"no"<<endl;
+	  }
+	}
+	return 0;
+}
