7db15a2058d1204ca484839ce4dd280bb92ac2ff
Pr-Parth <89503982+Pr-Parth@users.noreply.github.com>
2023-09-02 04:18:13 +0530

    Add files via upload

---
+++ ccChefvsDoof.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n;
+	    cin >> n;
+	    
+	    int a[n];
+	    for(int i=0; i<n; i++)
+	    {
+	        cin >> a[i];
+	    }
+	    int check=1;
+	    for(int i=0; i<n; i++)
+	    {
+	        if(a[i]%2==0)
+	        {
+	            check=0;
+	        }
+	    }
+	    if(check)
+	    {
+	        cout << "YES" << endl;
+	    }
+	    else
+	    {
+	        cout << "NO" << endl;
+	    }
+	}
+	return 0;
+}
