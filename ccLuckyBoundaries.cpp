75115621ad3e157f4368fc606b53b4d5b8675b97
Pr-Parth <89503982+Pr-Parth@users.noreply.github.com>
2023-09-03 10:33:57 +0530

    Add files via upload

---
+++ ccLuckyBoundaries.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+   cout<<n<<endl;
+}
+int main() {
+    int n;
+    cin>>n;
+    while(n--){
+        solve();
+    }
+	return 0;
+}
