c0e07092a3c0b8a5bdbeb873a93454ed27066bd6
Pr-Parth <89503982+Pr-Parth@users.noreply.github.com>
2023-09-08 21:30:16 +0530

    Add files via upload

---
+++ ccPlus2orMinus1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    
+	    if(n<1){
+	          cout<<1<<endl; 
+	    }
+	    else{
+	        cout<<3 * n<<endl;
+	    }
+	}
+	return 0;
+}
